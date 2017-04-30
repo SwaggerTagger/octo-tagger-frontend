12adeec370a883d26cddca4bfef26bc09ea83f2d
John Zielke <j.l.zielke@gmail.com>
2017-05-01 01:38:24 +0200

    Config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'linebreak-style':0
+    'linebreak-style':0,
+    'semi':[2,'never']
   }
 }
